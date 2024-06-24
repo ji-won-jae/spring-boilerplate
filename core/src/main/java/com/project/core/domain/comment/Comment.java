f4166bd23d73ea1337a3cc0c21533cd0a417b43e
ji-won-jae <ji-won-jae>
2024-06-24 16:54:14 +0900

    feat : Comment 엔티티 생성

---
+++ core/src/main/java/com/project/core/domain/comment/Comment.java
@@ -0,0 +1,32 @@
+package com.project.core.domain.comment;
+
+import com.project.core.domain.base.BaseTimeEntity;
+import com.project.core.domain.member.Member;
+import com.project.core.domain.post.Post;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @Column(length = 1000)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", updatable = false)
+    private Member writer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", updatable = false)
+    private Post post;
+
+}
