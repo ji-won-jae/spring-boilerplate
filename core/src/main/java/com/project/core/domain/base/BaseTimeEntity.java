aa8a657767d7e187536b2284bdb373e5ecc3f9a1
ji-won-jae <ji-won-jae>
2024-06-24 16:52:44 +0900

    feat : BaseTimEntity 생성

---
+++ core/src/main/java/com/project/core/domain/base/BaseTimeEntity.java
@@ -0,0 +1,27 @@
+package com.project.core.domain.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    // 생성한 날짜
+    @CreatedDate
+    @Column(updatable = false) // 해당 컬럼은 수정되지 않는다는 의미
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime modifiedDate;
+}
