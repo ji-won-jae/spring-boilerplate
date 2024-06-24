2baf2e26b7fdbf4d112d6ac73b0a815a87b295fb
ji-won-jae <ji-won-jae>
2024-06-24 16:56:03 +0900

    feat : DateTimeConfig 적용

---
+++ api/src/main/java/com/project/api/config/common/format/DateTimeConfig.java
@@ -0,0 +1,26 @@
+package com.project.api.config.common.format;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
+
+
+//LocalDateTIme 공통 포맷 적용
+@Configuration
+public class DateTimeConfig {
+    private static final String dateFormat = "yyyy-MM-dd";
+    private static final String dateTimeFormat = "yyyy-MM-dd HH:mm:ss.SSS";
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer dateJsonCustomizer() {
+        return builder -> {
+            builder.simpleDateFormat(dateTimeFormat);
+            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)));
+            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
+        };
+    }
+}
