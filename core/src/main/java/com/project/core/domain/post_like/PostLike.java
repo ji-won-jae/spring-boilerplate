2245e6409131e68d467268de870c9fa6827204c7
won-jae <jwo9767@naver.com>
2024-06-27 16:26:53 +0900

    feat: PostLike 초기 엔티티 생성

---
+++ core/src/main/java/com/project/core/domain/post_like/PostLike.java
@@ -0,0 +1,17 @@
+package com.project.core.domain.post_like;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_like_id")
+    private Long id;
+}
